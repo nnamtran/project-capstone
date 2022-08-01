a2bb5a204d7629ed8d3ce159a2f8029c4b6ac6fd
Nam Tran <nnamtran116@gmail.com>
2022-08-01 22:13:13 +1000

    some fixes

--- App.js
+++ App.js
@@ -1,33 +1,37 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Text, View, StyleSheet, Button, StatusBar, Image } from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
-  const [barcode, setBarcode] = useState('049000067316')
+  const [barcode, setBarcode] = useState('')
   const [title, setTitle] = useState('');
   const [image, setImage] = useState('...Loading');
 
   // Request API
   useEffect(() => {
-    const options = {
-      method: 'GET',
-      headers: {
-        'X-RapidAPI-Key': 'd5a9107caamsh123572a81b3a3b1p1f68a9jsn59b2f9b4d19e',
-        'X-RapidAPI-Host': 'barcodes1.p.rapidapi.com'
-      }
-    };
-    
+    if (barcode === '') {
+      return
+    } else {
+      const options = {
+        method: 'GET',
+        headers: {
+          'X-RapidAPI-Key': 'd5a9107caamsh123572a81b3a3b1p1f68a9jsn59b2f9b4d19e',
+          'X-RapidAPI-Host': 'barcodes1.p.rapidapi.com'
+        }
+      };
+      
     fetch('https://barcodes1.p.rapidapi.com/?query=' + parseInt(barcode), options)
-      .then(response => response.json())
-      .then((responseJson) => {
-        console.log(responseJson)
-        setTitle(responseJson['product']['title'])
-        setBarcode(responseJson['product']['barcode_formats']['ean_13'])
-        setImage(responseJson['product']['images'][0])
-      })
-      .catch(err => console.error(err));
+        .then(response => response.json())
+        .then((responseJson) => {
+          console.log(responseJson)
+          setTitle(responseJson['product']['title'])
+          setBarcode(responseJson['product']['barcode_formats']['ean_13'])
+          setImage(responseJson['product']['images'][0])
+    })
+    .catch(err => console.error(err));
+    }
   }, [barcode]);
 
 
@@ -78,22 +82,34 @@ export default function App() {
       
       <Text style={styles.maintext}>{barcode}</Text>
 
-      {scanned && <Button title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
+      {scanned && <Button style={{}} title={'Scan again?'} onPress={() => setScanned(false)} color='tomato' />}
 
       {/* Showing product */}
-      <View style={styles.container}>
+      {barcode === '' ? (
+        <View style={styles.barcodebox}>
         <View>
-          <Text style={{margin:10, fontSize:16}}>Product:</Text>
-          <Text style={{margin:10, fontSize:22}}>{title}</Text>
-          <Image source={{uri: image, width: 100, height: 100,}}></Image>
+          <Text style={{fontSize:22}}>Product:</Text>
+          <Text style={{fontSize:16}}>N/A</Text>
         </View>
       </View>
+      ) : (
+        <View style={styles.barcodebox}>
+        <View>
+          <Text style={{fontSize:22}}>Product:</Text>
+          <Text style={{fontSize:16}}>{title}</Text>
+          <Image source={{uri: image, width: 100, height: 100}}></Image>
+        </View>
+      </View>
+      ) 
+      }
+      
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    marginTop: 120,
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
@@ -101,7 +117,8 @@ const styles = StyleSheet.create({
   },
   maintext: {
     fontSize: 16,
-    margin: 20,
+    margin: 10,
+
   },
   barcodebox: {
     alignItems: 'center',
@@ -110,6 +127,6 @@ const styles = StyleSheet.create({
     width: 300,
     overflow: 'hidden',
     borderRadius: 30,
-    backgroundColor: 'tomato'
+    backgroundColor: 'white'
   }
 });
